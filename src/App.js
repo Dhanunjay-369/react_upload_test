c13f985d36fc49801beb9e1a1ba86a4794d3cd64
dhanunjayareddy kurakula <dhanunjayareddykurakula@gmail.com>
2023-10-14 20:28:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {Calc} from './components/calc.js';
+function App() {
+  return (
+    <div className="App">
+      <Calc/>
+    </div>
+  );
+}
+
+export default App;
